e9567ef8fe73db8580de9d58158772f5352ab1d8
Manos Papadakis <papadakm95@gmail.com>
2025-04-14 17:02:51 +0300

    Update Str.h

--- inst/include/Str.h
+++ inst/include/Str.h
@@ -5,7 +5,7 @@
 using namespace Rcpp;
 
 namespace Str {
-    std::string trim(std::string s);
+    std::string replaceSubstring(std::string s, std::string oldSubstr, std::string newSubstr);
     bool startsWith(std::string str, std::string substr);
     bool endsWith(std::string str, std::string substr);
     std::map<std::string, double> toTable(std::string x);
